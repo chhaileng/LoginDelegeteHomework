a8813907cfdecb510567223a2d041ecacd13cfe0
Chhaileng Peng <chhailengpeng@gmail.com>
2018-11-08 13:47:08 +0700

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Chhaileng Peng on 11/8/18.
+//  Copyright © 2018 Chhaileng Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
